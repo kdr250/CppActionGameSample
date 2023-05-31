595d2e1b8dbc2ec1b6198599594f6553b058d588
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-06-01 08:34:09 +0900

    コメント追加

--- src/Physics.cpp
+++ src/Physics.cpp
@@ -1,10 +1,17 @@
 #include "Physics.h"
 
+/**
+   return the overlap rectangle size of the bouding box of entity a and b
+*/
 Vec2 Physics::getOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
 {
     return Vec2();
 }
 
+/**
+   return the previous overlap rectangle size of the bouding boxes of entity a and b
+   previous overlap uses the entity's previous position
+*/
 Vec2 Physics::getPreviousOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
 {
     return Vec2();
