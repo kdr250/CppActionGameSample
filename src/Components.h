307aff318d1c2df6a0b2c4fea918f2eabe7f0cb6
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-05-31 10:11:10 +0900

    Componentsを作成

---
+++ src/Components.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Vec2.h"
+
+class CTransform
+{
+public:
+    Vec2 position         = {0.0, 0.0};
+    Vec2 previoutPosition = {0.0, 0.0};
+    Vec2 velocity         = {0.0, 0.0};
+    Vec2 scale            = {1.0, 1.0};
+    double angle          = 0;
+
+    CTransform(const Vec2& p) : position(p) {}
+};
+
+class CLifespan
+{
+public:
+    int remaining = 0;
+    int total     = 0;
+
+    CLifespan(int total) : remaining(total), total(total) {}
+};
+
+class CInput
+{
+public:
+    bool up    = false;
+    bool left  = false;
+    bool right = false;
+    bool down  = false;
+    bool shoot = false;
+
+    CInput() {};
+};
+
+class CBoundingBox
+{
+};
+
+class CAnimation
+{
+};
+
+class CGravity
+{
+};
+
+class CState
+{
+};
