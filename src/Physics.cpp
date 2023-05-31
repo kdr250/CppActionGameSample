91ae56a5260327d6c82d6e7cb5745207baf6edd2
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-06-01 08:29:06 +0900

    Physics作成

---
+++ src/Physics.cpp
@@ -0,0 +1,11 @@
+#include "Physics.h"
+
+Vec2 Physics::getOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
+{
+    return Vec2();
+}
+
+Vec2 Physics::getPreviousOverlap(std::shared_ptr<Entity> a, std::shared_ptr<Entity> b)
+{
+    return Vec2();
+}
