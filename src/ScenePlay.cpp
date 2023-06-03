0068482f3598760a0ac904c0701a7868eec02dee
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-06-03 17:58:36 +0900

    ScenePlay作成

---
+++ src/ScenePlay.cpp
@@ -0,0 +1,25 @@
+#include "ScenePlay.h"
+
+void ScenePlay::init(const std::string& levelPath) {}
+
+void ScenePlay::loadLevel(const std::string& filename) {}
+
+void ScenePlay::update() {}
+
+void ScenePlay::sAnimation() {}
+
+void ScenePlay::sMovement() {}
+
+void ScenePlay::sEnemySpawner() {}
+
+void ScenePlay::SCollision() {}
+
+void ScenePlay::sRender() {}
+
+void ScenePlay::sDoAction(Action& action) {}
+
+void ScenePlay::sDebug() {}
+
+void ScenePlay::onEnd() {}
+
+void ScenePlay::sDoAction(const Action& action) {}
---
+++ src/ScenePlay.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Scene.h"
+
+class ScenePlay : public Scene
+{
+    struct PlayerConfg
+    {
+        float X, Y, CX, CY, SPEED, MAXSPEED, JUMP, GRAVITY;
+        std::string WEAPONS;
+    };
+
+protected:
+    std::shared_ptr<Entity> m_player;
+    std::string m_levelPath;
+    PlayerConfg m_playerConfig;
+    bool m_drawTextures   = true;
+    bool m_drawCollision  = false;
+    bool m_drawGrid       = false;
+    const Vec2 m_gridSize = {64, 64};
+    sf::Text m_gridText;
+
+    void init(const std::string& levelPath);
+
+    void loadLevel(const std::string& filename);
+
+public:
+    void update();
+
+    void sAnimation();
+    void sMovement();
+    void sEnemySpawner();
+    void SCollision();
+    void sRender();
+    void sDoAction(Action& action);
+    void sDebug();
+
+    virtual void onEnd() override;
+    virtual void sDoAction(const Action& action) override;
+};
