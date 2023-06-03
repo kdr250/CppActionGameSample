a230a00e6b98681da700912fbd2a6f49b6d73aa2
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-06-03 15:35:29 +0900

    EntityManager作成実装

---
+++ src/EntityManager.cpp
@@ -0,0 +1,50 @@
+#include "EntityManager.h"
+
+void EntityManager::removeDeadEntities(EntityVector& vec)
+{
+    auto result = std::remove_if(vec.begin(),
+                                 vec.end(),
+                                 [](std::shared_ptr<Entity>& entity)
+                                 {
+                                     return !entity->isActive();
+                                 });
+    vec.erase(result, vec.end());
+}
+
+EntityManager::EntityManagert() {}
+
+void EntityManager::update()
+{
+    for (auto entity : m_entitiesToAdd)
+    {
+        m_entities.push_back(entity);
+        m_entityMap[entity->tag()].push_back(entity);
+    }
+
+    m_entitiesToAdd.clear();
+
+    removeDeadEntities(m_entities);
+
+    for (auto& [tag, vec] : m_entityMap)
+    {
+        removeDeadEntities(vec);
+    }
+}
+
+std::shared_ptr<Entity> EntityManager::addEntity(const std::string& tag)
+{
+    auto entity = std::make_shared<Entity>(m_totalEntities, tag);
+    m_entitiesToAdd.push_back(entity);
+    m_totalEntities++;
+    return entity;
+}
+
+EntityVector& EntityManager::getEntities()
+{
+    returtn m_entities();
+}
+
+EntityVector& EntityManager::getEntities(const std::string& tag)
+{
+    return m_entityMap[tag];
+}
---
+++ src/EntityManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <vector>
+#include "Entity.h"
+
+typedef std::vector<std::shared_ptr<Entity>> EntityVector;
+
+class EntityManager
+{
+private:
+    EntityVector m_entities;                          // all entities
+    EntityVector m_entitiesToAdd;                     // entities to add next update
+    std::map<std::string, EntityVector> m_entityMap;  // map from entity tag to vectors
+    size_t m_totalEntities = 0;
+
+    void removeDeadEntities(EntityVector& vec);
+
+public:
+    EntityManagert();
+
+    void update();
+    std::shared_ptr<Entity> addEntity(const std::string& tag);
+    EntityVector& getEntities();
+    EntityVector& getEntities(const std::string& tag);
+};
